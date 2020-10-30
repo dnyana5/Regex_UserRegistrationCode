7aec06da29daad06aa06ca5fc1b9b89322f3d8b7
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-30 20:58:17 +0530

    Added:First Name Validation Code UC1

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration Program Using Regex on master branch ."
+
+#First name starts with Cap and has minimum 3 characters
+read -p "User First Name : " firstName
+pattern1="^([A-Z]{1})[a-zA-Z]{2,}$"
+if [[ $firstName =~ $pattern1 ]]
+then
+        echo "valid First name"
+else
+        echo "invalid First name"
+fi
+
