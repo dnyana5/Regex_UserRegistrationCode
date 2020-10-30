8b5a45f0aa83bc0525d84b37ee76da151e093901
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2020-10-30 21:11:42 +0530

    Added:Last Name validation code UC2

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -12,3 +12,12 @@ else
         echo "invalid First name"
 fi
 
+#Last name starts with Cap and has minimum 3 characters
+read -p "User Last Name : " lastName
+pattern2="^[A-Z]{1}[a-zA-Z]{2,}$"
+if [[ $lastName =~ $pattern2 ]]
+then
+        echo "valid Last name"
+else
+        echo "invalid Last name"
+fi
